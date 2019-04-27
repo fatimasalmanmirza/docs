ae299686ba3b53db1d210f111f1e384f84d69dd3
Fatima <fatima@Muhammads-MacBook-Pro.local>
2019-04-26 23:31:36 -0700

    some more refactoring

--- model.py
+++ model.py
@@ -1,84 +1,97 @@
 from flask import Flask, render_template, request, jsonify
 import requests
+import logging
 app = Flask(__name__)
 
 
 app.secret_key = "ABC"
+base_url = "https://api.yelp.com/v3/"
+api_key ="k9VGFeAw2G7hlaSzDbt2bM9corS2Yvll4zQyATsUTvo9PmUfM3HzJ6EKV1fA5k83hBwZkXU04Il0qTh4PO64LsPqPLL5KsNGnIy0bs0jyE1KS2PDKGYItIGcMxhdXHYx"
+logger = logging.getLogger(__name__)
 
 
+def get_doctor_detail(id):
+    """
+    Make request to Yelp API to get doctor detail object
+    """
+    headers = {"Authorization": 'Bearer ' + api_key}
+    return requests.get(
+        f"{base_url}businesses/{id}",
+        headers=headers).json()
 
+def get_doctors(term, location, category=""):
+    """
+    Make request to YELP API to get all doctors.
+    """
+    headers = {
+        "Authorization": 'Bearer ' + api_key
+    }
+
+    payload = {
+        "term": "doctors", 
+        "location": location, 
+        "sort_by": "rating", 
+        "categories":category
+    }
+
+    r = requests.get(
+        f"{base_url}businesses/search",
+        headers=headers, 
+        params=payload
+    ).json()
+    
+    return r["businesses"]
+
+
+def unique_elements(elements):
+    result = set()
+    for elem in elements:
+        result.add(elem)
+    return list(result)
 
 @app.route("/",methods=["GET","POST"])
 def show_all_doctors():
     """Display all doctors of a specific location to user"""
     if request.method == "GET":
-        location_docs = "San francisco"
-        category = "familydr"
-        
+        location = "San Francisco, CA"
+        category=""
     else:
         args = request.form
-        location_docs = args["location"]
+        location = args["location"]
         category = args["speciality"]
 
-    key ="k9VGFeAw2G7hlaSzDbt2bM9corS2Yvll4zQyATsUTvo9PmUfM3HzJ6EKV1fA5k83hBwZkXU04Il0qTh4PO64LsPqPLL5KsNGnIy0bs0jyE1KS2PDKGYItIGcMxhdXHYx"
-
-    headers = {"Authorization": 'Bearer ' + key}
-    payload = {"term": "doctors", "location": location_docs, "sort_by": "rating", "categories":category}
-    r = requests.get("https://api.yelp.com/v3/businesses/search",
-                     headers=headers, params=payload)
-    doctors_info = r.json()
-    list_doctors_info = doctors_info["businesses"]
-    location = set()
-    speciality=set()
-    rating = set()
-    for doc in list_doctors_info:
-        x = doc["categories"][0]["alias"]
-        y = "/n".join(doc["location"]["display_address"])
-        z = doc["rating"]
-        speciality.add(x)
-        location.add(y)
-        rating.add(z)
-
-    doc_speciality = list(speciality)
-    location = ["San Franciso, CA", "Los Angeles, CA", "New York City, NY"]
-    rating = list(rating)
+    doctors = get_doctors("doctors", location, category)
+    specialities = unique_elements([doc["categories"][0]["alias"] for doc in doctors])
+    locations = ["San Franciso, CA", "Los Angeles, CA", "New York City, NY"]
+    
     return render_template("index.html",
-        list_doctors_info=list_doctors_info,
-        doc_speciality=doc_speciality,
-        selected_location=location_docs, 
-        location=location,
-        rating=rating)    
+        list_doctors_info=doctors,
+        doc_speciality=specialities,
+        selected_location=location, 
+        location=locations)    
 
-
+    
 @app.route("/doctor/<id>")
-def doc_details(id):
-    key ="k9VGFeAw2G7hlaSzDbt2bM9corS2Yvll4zQyATsUTvo9PmUfM3HzJ6EKV1fA5k83hBwZkXU04Il0qTh4PO64LsPqPLL5KsNGnIy0bs0jyE1KS2PDKGYItIGcMxhdXHYx"
-    location_docs = "San francisco"
-    headers = {"Authorization": 'Bearer ' + key}
-    r = requests.get(f"https://api.yelp.com/v3/businesses/{id}",
-                     headers=headers)
-    doctor = r.json()
+def doc_details(id):    
+    """
+    See Doctor details with similar doctors based on 
+    current doctor's categories
+    """
+    doctor = get_doctor_detail(id)
 
-
-    location_docs = "San francisco"
-    headers = {"Authorization": 'Bearer ' + key}
+    location = doctor["location"]["city"]
+    print(f"getting similar doctors in {location}")
     category = doctor["categories"][0]["alias"]
     category_name = doctor["categories"][0]["title"]
-    payload = {"categories": category, "location": location_docs, "sort_by": "rating"}
-    r = requests.get("https://api.yelp.com/v3/businesses/search",
-                     headers=headers, params=payload)
-    doctors_info = r.json()
-    list_doctors_info = doctors_info["businesses"]
-    return render_template("about.html",doctor=doctor, category=category, category_name=category_name, list_doctors_info=list_doctors_info)
-
-
-
+    similar_doctors = get_doctors("doctors", location=location, category=category)
+    return render_template("about.html",
+        doctor=doctor, 
+        category=category, 
+        category_name=category_name, 
+        list_doctors_info=similar_doctors,
+        location=location
+    )
 
 if __name__ == "__main__":
-
     app.debug = True
-
-
-
-    # DebugToolbarExtension(app)
     app.run(host="0.0.0.0", port=6001)
